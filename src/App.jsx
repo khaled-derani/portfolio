6dbc511fa5b2a3118f43e89417a6137a4cb4b980
khaled-derani <deranik86@gmail.com>
2026-01-17 12:02:00 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+import Navbar from "./Sections/Navbar";
+
+function App() {
+  // const [hey, setHey] = useState();
+  console.log();
+  const khaled = "";
+  console.log(khaled);
+  return (
+    <div className="relative w-screen min-h-screen overflow-x-auto">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sections/Navbar.jsx
@@ -0,0 +1,7 @@
+function Navbar() {
+  return (
+    <nav className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"></nav>
+  );
+}
+
+export default Navbar;
